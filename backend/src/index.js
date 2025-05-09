66ff62fb0459b6bd5f093c78e853f2c1bda493b8
Gajendra-Rao <gajendrapawar69@gmail.com>
2025-05-09 15:16:19 +0530

    signup added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,26 @@
 import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json()); //extract json data from body
+app.use(cookieParser()); 
 
 app.get('/', (req,res)=>{
     res.status(200).send("Wassup Nigga!");
 });
 
+app.use("/api/v1/auth", authRoutes);
+
+
 app.listen(PORT, ()=>{
     console.log(`server is running on the port ${PORT}`);
+    connectDB();
     
 })
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -14,5 +14,15 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "",
     }
-})
+}, {timestamps: true});
+
+
+//mongoDB will store "User" in users automatically
+const User = mongoose.model("User", userSchema);
+
+export default User;
