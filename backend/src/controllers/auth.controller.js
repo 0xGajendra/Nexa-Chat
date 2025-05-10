ed618a6183c527df4836b7582898326ca26e42c1
Gajendra-Rao <gajendrapawar69@gmail.com>
2025-05-10 17:11:40 +0530

    update pfpa dn check auth added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../config/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt"
@@ -77,6 +78,30 @@ export const logout = (req, res) => {
     }
 }
 
-export const updateProfile = async (res, res) => {
-    
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePicture} = req.body;
+        const userId = req.user._id; //this is possible becuase of verifyToken middleware
+
+        if(!profilePicture){
+            return res.status(400).json({message: "Profile pic is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePictconsture);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePicture: uploadResponse.secure_url},{new: true}) //new:true returns the updated User not the old one(by default)
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+        console.log("error in update profile", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal server error"}); 
+    }
 }
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -9,7 +9,21 @@ export const verifyToken = async (req, res, next)=> {
         if(!token){
             return res.status(401).json({success: false, message: "unauthorized - No Token Provided"});
         }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(!decoded){
+            return res.status(400).json({ message: "Unauthorized - Invalid Token"});
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+        if(!user)
+        {
+            return res.status(400).json({ message : "User not found"});
+        }
+        req.user = user;
+        next();
     } catch (error) {
+        console.log("Error in verifyToken middleware", error.message);
+        res.status(500).json({message: "Internal server error"});
         
     }
 }
