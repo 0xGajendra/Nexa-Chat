372e1580c5212ae2809bd81f0f2a787c97e8715a
Gajendra-Rao <gajendrapawar69@gmail.com>
2025-05-20 11:09:18 +0530

    jwt ahh fix

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,8 +2,6 @@ import jwt from "jsonwebtoken";
 export const generateToken = (userId, res)=> {
     const token = jwt.sign({userId}, process.env.JWT_SECRET,{
         expiresIn: "7d",
-        sameSite: "None",
-        secure: true
     });
     res.cookie("token", token, {
         maxAge: 7*24*60*60*1000, //in ms
