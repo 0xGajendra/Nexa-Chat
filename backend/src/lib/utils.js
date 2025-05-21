4992a3d676b4110f701089bd2daf1d8da2198009
Gajendra-Rao <gajendrapawar69@gmail.com>
2025-05-21 09:32:42 +0530

    this maybe fix the production issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,9 +1,13 @@
 import jwt from "jsonwebtoken";
-export const generateToken = (userId, res)=> {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
-        expiresIn: "7d",
-    });
-    res.cookie("token", token, {
-        maxAge: 7*24*60*60*1000, //in ms
-    })
-}
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("token", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, //in ms
+    httpOnly: true, // can't access via JS
+    secure: process.env.NODE_ENV === "production", // true in prod
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  });
+};
